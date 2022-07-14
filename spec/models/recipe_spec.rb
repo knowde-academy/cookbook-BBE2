55bde700b6cbfb9cefc75887dbcb3f2fdb784cf7
eryk kosinski <kosssa98@gmail.com>
2022-07-14 10:34:31 +0000

    Modify RecipeSpec checked validate and if cooking_time is int and >0

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -4,5 +4,9 @@ RSpec.describe Recipe, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
   end
+  describe 'check if cooking_time is integer and >0' do
+    it { is_expected.to validate_numericality_of(:cooking_time).only_integer.is_greater_than(0) }
+  # it {expect(:cooking_time).to be_a_kind_of(Integer)}
+  end
   
 end

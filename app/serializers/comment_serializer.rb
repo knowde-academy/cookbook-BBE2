671a61246f9b481ae3fa232fffa0f5ef6838db44
eryk kosinski <kosssa98@gmail.com>
2022-07-14 08:15:43 +0000

    Added anon variable

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,8 +1,8 @@
 class CommentSerializer < ActiveModel::Serializer
   attributes :id, :author, :body, :created_at
-
+ ANON = 'annonymous'
   def author
-    return 'annonymous' unless object.author
+    return ANON unless object.author
 
     object.author
   end

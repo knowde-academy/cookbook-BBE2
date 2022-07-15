c79a4464c4a52550d2af7d9916b5713c92112505
Paweł Król <p.krol18a@gmail.com>
2022-07-15 07:24:05 +0000

    szybki zapis

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe < ApplicationRecord
   validates :name, presence: true
-  validates :level, allow_nil: true, inclusion: { in: 1..5, 
-    message: 'Level must be integer in range 1-5 inclusive'}
+  validates :level, allow_nil: true, 
+                    inclusion: { in: 1..5,
+                                message: 'Level must be integer in range 1-5 inclusive' }
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe Recipe, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to allow_value(nil).for(:level) }
-    it { is_expected.to validate_inclusion_of(:level).in_range(1..5) }
+    it { is_expected.to validate_inclusion_of(:level, message: "Level must be integer in range 1-5 inclusive").in_range(1..5) }
   end
 end

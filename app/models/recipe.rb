8169045f34bd845fe23b24aac9a132698da87ee2
Paweł Król <p.krol18a@gmail.com>
2022-07-18 09:20:27 +0000

    Added tests for validations and associations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ApplicationRecord
-  has_many :rates
+  has_many :rates, dependent: :destroy
   
   validates :name, presence: true
   validates :video_link, allow_blank: true, url: true
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
+  describe 'associations' do
+    it { is_expected.to have_many(:rates).dependent(:destroy) }
+  end
+  
   describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to allow_value('', nil).for(:video_link) }

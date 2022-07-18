8f17bf8ebe4fc79afc1368ba5a8111a9dc0f343a
Paweł Król <p.krol18a@gmail.com>
2022-07-18 08:41:22 +0000

    Fixed database schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_15_084819) do
+ActiveRecord::Schema.define(version: 2022_07_14_114653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "rates", force: :cascade do |t|
-    t.bigint "recipe_id", null: false
-    t.integer "vote", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["recipe_id"], name: "index_rates_on_recipe_id"
-  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "name", null: false

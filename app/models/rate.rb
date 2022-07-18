75da6a0cbb5b07f01138dabaacc2249099b345d6
Paweł Król <p.krol18a@gmail.com>
2022-07-18 08:57:41 +0000

    Created rates validations and associations

---
+++ app/models/rate.rb
@@ -0,0 +1,8 @@
+class Rate < ApplicationRecord
+  INCLUSION_ERROR_MESSAGE = 'must be integer in range 1-5 inclusive'.freeze
+  
+  belongs_to :recipe
+  
+  validates :vote, presence: true,
+            inclusion: {in: 1..5, message: INCLUSION_ERROR_MESSAGE }
+end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ApplicationRecord
+  has_many :rates
+  
   validates :name, presence: true
   validates :video_link, allow_blank: true, url: true
 end

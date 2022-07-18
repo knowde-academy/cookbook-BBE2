65e4ef6ffd27bbf4e00c1f933d582a228318f690
eryk kosinski <kosssa98@gmail.com>
2022-07-18 11:40:47 +0000

    DB Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2022_07_14_114653) do
-
+ActiveRecord::Schema.define(version: 2022_07_15_070931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,8 +31,8 @@ ActiveRecord::Schema.define(version: 2022_07_14_114653) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "video_link"
     t.integer "cooking_time"
+    t.decimal "price"
     t.integer "level"
-
   end
 
   add_foreign_key "comments", "recipes"

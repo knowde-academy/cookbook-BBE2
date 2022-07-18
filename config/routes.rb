f4c6bd012de45c78784ab75b07f9043172ff9ec4
eryk kosinski <kosssa98@gmail.com>
2022-07-18 10:18:45 +0000

    Config Initializer Settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for '[USER_CLASS]', at: '[MOUNT_PATH]'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do

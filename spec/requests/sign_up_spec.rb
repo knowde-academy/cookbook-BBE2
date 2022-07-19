4b4e54a9aa04787e57ae5acd6af26bebe6f607d9
eryk kosinski <kosssa98@gmail.com>
2022-07-19 11:06:35 +0000

    Create SignUpSpec

---
+++ spec/requests/sign_up_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+RSpec.describe '#sign_in' do
+  let(:password) {'password'}
+  let(:user) {build(:user, password: password)}
+    let(:register) do
+    post '/api/v1/auth/', params: { email: user.email, password: password}
+  end
+  
+  context ' with valid params' do
+    it do
+      expect do
+        register
+      end.to change(User, :count).by(1)
+    end
+    
+    it 'request successful' do
+      register
+      expect(response).to have_http_status(200)
+      end
+    end
+
+  context 'with invalid params' do
+    let(:email) {'aaaa'}
+    let(:user) { build( :user, email: email)}
+    it do
+      expect do
+        register
+      end.not_to change( User, :count)
+    end
+    it 'request successful' do
+      register
+        expect(response).to have_http_status(422)
+    end
+  end
+end
